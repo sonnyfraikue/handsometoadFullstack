2569d04ee30caed9694a67819e7b5b081912f792
Sonny Peter Fraikue <sonnuyfraikue@gmail.com>
2015-03-13 00:03:52 +0000

    building forms to create products and templates

--- dist/api/vendor/autoload.php
+++ dist/api/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit14fccb91a8237ef6a0c88d08495202bf::getLoader();
+return ComposerAutoloaderInit61c4e4f2892561f33685c9f609885566::getLoader();
