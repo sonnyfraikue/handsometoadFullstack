320e543ba865d3da0da26fb953cbe6b1f40763e2
Sonny Fraikue <sonnyf@glaceon.local>
2015-04-28 14:53:31 +0100

    applied patch to grunt-connect-proxy

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,10 +93,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35829
       },
       proxies: [
         {
@@ -151,7 +151,7 @@ module.exports = function (grunt) {
     // PHP built-in server
     php: {
       options: {
-        port: 8000,
+        port: 8010,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '127.0.0.1',
         router: 'index.php'
